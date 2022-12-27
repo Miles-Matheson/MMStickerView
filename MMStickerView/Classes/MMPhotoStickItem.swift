c62196ac61ea941bd3ac3c3fe9091e2bb79903b2
Miles <liyida188@163.com>
2022-12-27 15:34:41 +0800

    update

---
+++ MMStickerView/Classes/MMPhotoStickItem.swift
@@ -0,0 +1,18 @@
+//
+//  MMPhotoStickItem.swift
+//  ImageCut
+//
+//  Created by Miles on 2022/12/2.
+//
+
+import UIKit
+
+class MMPhotoStickItem{
+    
+    init(image:UIImage,section:Int){
+        self.image = image
+        self.section = section
+    }
+    var image:UIImage
+    var section:Int    
+}
